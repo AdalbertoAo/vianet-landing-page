3e6efb718edeaf565b8d2c9855a82ebe51a3ab53
AdalbertoAo <4dalbertosil@gmail.com>
2026-01-20 17:44:54 +0100

    Merge branch 'main' do repositorio na nuvem

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Button from "./components/Button.jsx"
 
 import NavBar from "./components/NavBar"
 
- feacture-navbar
+
 
 function App() {
 
@@ -12,28 +12,16 @@ function App() {
     <div className="bg-blue-950">
     
       <Button variant="primary">Primary Button</Button>
-    <Button variant="secundary">Secundary Button</Button>
-    <Button> botao 3</Button>
-      
-     
-    </div>
-
-    <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <h1>Landing page vianet</h1>
-      <p>i love coding kkkk</p>
-=======
-      <Button variant="primary">Primary Button</Button>
       <Button variant="secundary">Secundary Button</Button>
->>>>>>> feature-button
-=======
       <NavBar />
       <h1>Landing page vianet</h1>
       <p>Esta é a landing page da Vianet.</p>
->>>>>>> feacture-navbar
-    </>
->>>>>>> cc3274de1f00cdb8bb6a49fde639cea37457054d
+    </div>
+
+     
+
+
+
   )
 }
 
